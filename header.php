14be6e85e87963c79c462e618213ce836f6a6c00
seenia <30547424+seenia@users.noreply.github.com>
2017-09-11 13:40:01 +0530

    edited

--- header.php
+++ header.php
@@ -55,18 +55,18 @@
     
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav navbar-left">
+    
         
         	<?php if ( has_nav_menu( 'header-menu' ) ) : ?>
             <?php
 				wp_nav_menu( array( 
-                        'theme_location'    => 'header-menu') );
+                        'theme_location'    => 'header-menu', 
+                        'menu_class'        => 'nav navbar-nav navbar-left' ) );
             ?>
             <?php else : ?>
 				<li ><a  href=" <?php echo esc_url(admin_url( 'nav-menus.php' ));?>  "><?php echo __( 'Add a Primary Menu', 'dblogger' );?>  </a></li>
 			<?php endif; ?>
         
-       </ul>
                 <ul class="navbar-right social-links-top ">
                  <?php
                  if ( $socials = get_theme_mod( 'social' ) ) 
@@ -82,7 +82,6 @@
                         <form id="search" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get"> 
   
                             <input type="text"  placeholder="<?php echo esc_attr_x( 'Search...&hellip;', 'placeholder', 'dblogger' ); ?>"  value="<?php echo get_search_query(); ?>" name="s" size="40"/>
-          
                         </form>
                     </li>
                 </ul>
