04dfd99f4222127594fba1e33d231232f1f083ce
Rijo Abraham <30547872+rijoabraham@users.noreply.github.com>
2017-10-09 11:12:28 +0530

    update

--- template-parts/content.php
+++ template-parts/content.php
@@ -16,12 +16,12 @@
             {
              the_post_thumbnail('dblogger_recent_post'); 
             }else{?>
-            <img src="<?php echo get_template_directory_uri()?>/img/p-2.jpg" alt="" class="img-responsive">
+            <img src="<?php echo get_template_directory_uri()?>/img/default.jpg" alt="" class="img-responsive">
             <?php }?>
           </a>
         <header class="entry-header"><a href="<?php the_permalink();?>">
           <h5><?php the_title();?></h5>
-          </a> <span class="date-article"><?php get_option('d F Y');?></span><!-- in <span class="byline"><span class="author vcard"><a href="#">Wordpress . Blog</a></span></span> --></header>
+          </a> <span class="date-article"><?php get_option('d F Y');?></span></header>
         <p> <?php echo the_excerpt();?></p>
       </article>
       <!--/blog post--> 
