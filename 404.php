e0d16c7c9a2bbcd7f11d2533554345833febaf79
seenia <30547424+seenia@users.noreply.github.com>
2017-10-17 17:33:14 +0530

    edited

--- 404.php
+++ 404.php
@@ -1,54 +1,47 @@
 <?php
-/**
- * The template for displaying 404 pages (not found)
- *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
- *
- * @package dblogger
- */
-
+	/**
+	 * The template for displaying 404 pages (not found)
+	 *
+	 * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+	 *
+	 * @package dblogger
+	 */
 get_header(); ?>
-
-<?php 
-    $background_img   = esc_url( get_theme_mod( 'dblogger_custom_img' ) );   
-    $background_img_static   = get_template_directory_uri()."/img/b-1.jpg";
-    $image = $background_img ? "$background_img" : "$background_img_static"; 
+<?php
+$background_img   = get_theme_mod( 'dblogger_custom_img' );
+$background_img_static   = get_template_directory_uri() . '/img/b-1.jpg';
+$image = $background_img ? "$background_img" : "$background_img_static";
 ?>
-<Section id="page-banner" style="background-image: url(<?php echo $image; ?>);">
-  <div class="overlay-banner">
-    <div class="content">
-      <div class="container"> 
-        <!--breadcrumb-->
-        <header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'dblogger' ); ?></h1>
-				</header><!-- .page-header -->
-
-        <!--/breadcrumb-->
-        
-        <h1><?php the_title(); ?> </h1>
-      </div>
+<section id="page-banner" style="background-image: url( <?php echo esc_url( $image ); ?> );">
+    <div class="overlay-banner">
+        <div class="content">
+            <div class="container">
+                <!--breadcrumb-->
+                <header class="page-header">
+                    <h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'dblogger' ); ?></h1>
+                </header><!-- .page-header -->
+                <!--/breadcrumb-->
+                <h1><?php the_title(); ?> </h1>
+            </div>
+        </div>
     </div>
-  </div>
-</Section>
+</section>
 
 <section id="Blog-home">
-  <div class="container">
-    <div class="row"> 
-
+	<div class="container">
+		<div class="row">
 			<section class="error-404 not-found">
-				
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'dblogger' ); ?></p>
-
-					<?php
-						get_search_form();
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-      </div>
-    </div>
+					<p>
+						<?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'dblogger' );?> 
+                  	</p>
+					<?php get_search_form();?>
+              	</div>
+				<!-- .page-content -->
+			</section>
+			<!-- .error-404 -->
+		</div>
+	</div>
 </section>
 
 <?php
--- footer.php
+++ footer.php
@@ -10,50 +10,40 @@
  */
 
 ?>
-<footer id="bottom-footer">
-  <div class="container">
-    <div class="row wow fadeInUp">
-      <div class="col-md-4"> 
-        <!--copyright-->
-        <p class="copyright">© 2017 Digital Blogger. All rights reserved</p>
-        <!--/copyright--> 
-      </div>
-      <!--bottom nav-->
-      <div class="col-md-4">
-        <nav class="bottom-nav">
-          <ul>
-              
-            <?php wp_nav_menu( array( 
-               'theme_location'    => 'footer-menu', 
-               'menu_class'        => 'bottom-nav' ) );
-            ?>  
-    
-              
-            <!--<li><a href="#">terms</a></li>
-            <li><a href="#">privacy</a></li>
-            <li><a href="#">copyright</a></li>
-            <li><a href="#">cookies</a></li>-->
-          </ul>
-        </nav>
-      </div>
-      <!--/bottom nav--> 
-      
-      <!--powered by-->
-      <div class="col-md-4"> 
-        
-        <!--copyright-->
-        <p class="powered-by">Made with <i class="fa fa-heart"></i> by <a href="#">Dcrazed</a></p>
-      </div>
-      <!--/powered by--> 
-      
-    </div>
-  </div>
-</footer>
-<?php wp_footer(); ?>
-
-<script>
-new WOW().init();
-</script>
-
-</body>
+		<footer id="bottom-footer">
+			<div class="container">
+				<div class="row wow fadeInUp">
+					<div class="col-md-4">
+						<!--copyright-->
+						<p class="copyright">© 2017 Digital Blogger. All rights reserved</p>
+						<!--/copyright--> 
+					</div>
+					<!--bottom nav-->
+					<div class="col-md-4">
+						<nav class="bottom-nav">
+							<ul>
+								<?php
+								wp_nav_menu( array(
+								'theme_location'    => 'footer-menu',
+								'menu_class'        => 'bottom-nav' ) 
+								);
+								?>
+							</ul>
+						</nav>
+					</div>
+				<!--/bottom nav-->
+				<!--powered by-->
+				<div class="col-md-4">
+				<!--copyright-->
+				<p class="powered-by">Made with <i class="fa fa-heart"></i> by <a href="#">Dcrazed</a></p>
+				</div>
+				<!--/powered by--> 
+				</div>
+			</div>
+		</footer>
+		<?php wp_footer(); ?>
+		<script>
+			new WOW().init();
+		</script>
+	</body>
 </html>
