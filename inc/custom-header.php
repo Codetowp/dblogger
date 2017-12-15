341c18dd51d141d38e568ac3ae8e79d5e53dc1c2
Rijo Abraham <30547872+rijoabraham@users.noreply.github.com>
2017-12-15 18:23:42 +0530

    updates

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -18,7 +18,7 @@
  */
 function dblogger_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'dblogger_custom_header_args', array(
-		'default-image'          => get_template_directory_uri() . '../assets/img/b-1.jpg',
+		'default-image'          => get_template_directory_uri() . '/assets/img/b-1.jpg',
 		'default-text-color'     => '000000',
 		'width'                  => 1900,
 		'height'                 => 290,
